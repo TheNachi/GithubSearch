4e009f1f9575cc2adafa022805ae89ee5bd40914
Munachimso Ugorji <ugorjimunachimso@gmail.com>
2023-11-26 22:32:09 +0100

    Initial Commit

---
+++ GithubSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubSearch
+//
+//  Created by Munachimso Ugorji on 11/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubSearch/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by Munachimso Ugorji on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
